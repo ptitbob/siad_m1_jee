b86a435e4af9582f30c2f79a1dcc362a3cf81a3a
Francois Robert <github@shipstone.org>
2014-10-09 00:56:05 +0200

    Ajout de la classe utilitaire pour JPA

---
+++ jpa/src/test/java/fr/univ/blois/siad/m1/javaee/jpa/common/SiadTest.java
@@ -0,0 +1,49 @@
+package fr.univ.blois.siad.m1.javaee.jpa.common;
+
+import org.junit.BeforeClass;
+import org.pmw.tinylog.Configurator;
+import org.pmw.tinylog.writers.ConsoleWriter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import static org.pmw.tinylog.Level.TRACE;
+
+
+/**
+ * @author: François Robert
+ */
+public class SiadTest {
+
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+
+	@BeforeClass
+	public static void setUp() {
+		Configurator.defaultConfig()
+			.writer(new ConsoleWriter())
+			.level(TRACE)
+			.activate();
+		entityManagerFactory = Persistence.createEntityManagerFactory("entityPU");
+		entityManager = entityManagerFactory.createEntityManager();
+	}
+
+	public static void tearDown() {
+		if (entityManager != null) {
+			entityManager.close();
+		}
+		if (entityManagerFactory != null) {
+			entityManagerFactory.close();
+		}
+	}
+
+	protected <E> E persist(E entity) {
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(entity);
+		entityTransaction.commit();
+		return entity;
+	}
+}
