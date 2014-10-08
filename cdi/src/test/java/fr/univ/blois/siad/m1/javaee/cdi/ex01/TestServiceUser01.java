322a589eb15681a9bc02d4c7ddfb7a5af4b75695
Francois Robert <github@shipstone.org>
2014-10-08 23:45:41 +0200

    TD - CDI - Ex 01

---
+++ cdi/src/test/java/fr/univ/blois/siad/m1/javaee/cdi/ex01/TestServiceUser01.java
@@ -0,0 +1,22 @@
+package fr.univ.blois.siad.m1.javaee.cdi.ex01;
+
+import fr.univ.blois.siad.m1.javaee.common.SiadTest;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author: François Robert
+ */
+public class TestServiceUser01 extends SiadTest {
+
+	@Test
+	public void validateInitializeRandomNumber() {
+		ServiceUser01 serviceUser01 = null;
+		assertNotNull("L'instance ne doit pas être nulle", serviceUser01);
+		assertNotNull("le nombre doit être affecté", serviceUser01.getRandomNumber());
+		assertTrue("Le nombre doit être compris de manière aléatoire entre 1 et 10", serviceUser01.getRandomNumber() > 1 && serviceUser01.getRandomNumber() <= 10);
+	}
+
+}
